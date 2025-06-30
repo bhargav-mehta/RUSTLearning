90b8a72f2fa563a51834f6c145a7fb26c40368f4
Bhargav Mehta <bhargav.m.mehta@gmail.com>
2025-07-01 00:24:43 +0200

    Using mutable variables to change values of variable

--- RustBasics/src/main.rs
+++ RustBasics/src/main.rs
@@ -3,6 +3,11 @@ fn main() {
     let y = 10;
     let sum = x + y;
     println!("The sum of {} and {} is {}", x, y, sum);
-    
+
+    let mut a = 15; // This line will cause an error because x is immutable
+    println!("The value of a is: {}", a);
+    a = 20; // This line is fine because a is mutable
+    println!("The value of a after change is: {}", a);
+        
     println!("Hello, This is Bhargav's first line in Rust !");
 }
