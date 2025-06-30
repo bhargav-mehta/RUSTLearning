f80de8fb2a79f1751b77987895b5644150cd9d9c
Bhargav Mehta <bhargav.m.mehta@gmail.com>
2025-07-01 00:20:49 +0200

    Use default variable declaration with addition operator and println macro

--- RustBasics/src/main.rs
+++ RustBasics/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, This is Bhargav's first line in Rust World!");
+    let x = 5;
+    let y = 10;
+    let sum = x + y;
+    println!("The sum of {} and {} is {}", x, y, sum);
+    
+    println!("Hello, This is Bhargav's first line in Rust !");
 }
