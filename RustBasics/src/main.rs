18fe3aaa685a835061666a0726f152e205a9c1ac
Bhargav Mehta <bhargav.m.mehta@gmail.com>
2025-07-01 08:00:08 +0200

    Function overloading does not work in RUST

--- RustBasics/src/main.rs
+++ RustBasics/src/main.rs
@@ -8,6 +8,14 @@ fn main() {
     println!("The value of a is: {}", a);
     a = 20; // This line is fine because a is mutable
     println!("The value of a after change is: {}", a);
-        
+
     println!("Hello, This is Bhargav's first line in Rust !");
+}
+
+fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+fn add(x: i32, y: i32, z: i32) -> i32 {
+   x + y + z
 }
